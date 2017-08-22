5589a1a2063e2868830eb41b8499f16f1bd7e537
Alan Ennen <aennen@amdocs.com>
2017-08-22 14:38:19 -0500

    Enhanced

--- routes/apis.js
+++ routes/apis.js
@@ -15,82 +15,127 @@ var proxyAgent = new HttpsProxyAgent(proxyOptions);
 
 module.exports = {
 
-    getPending : function(callback) {
-        var payload ="";
-
-        console.log("GETPENDING");
-
-        var options = {
-            host: 'amgate.itahs.com',
-            path: '/node/pending?appcode=amgate&mode=json',
-            method: 'GET',
-            agent: proxyAgent
-        };
-
-        https.request(options, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-            res.setEncoding('utf8');
-
-            res.on('data', function (chunk) {
-                payload += chunk;
-                console.log('PENDGIN-BODY: ' + chunk);
-            });
-
-            res.on('end', function() {
-                console.log("END");
-                callback(payload);
-            })
-
-        }).end(function() {
-            console.log("PENDGIN-END");
-        });
-    },
-
-    getActive : function (callback) {
+    getAccounts: function (accountType, callback) {
 
         console.log("GET-ACTIVE");
 
-        var payload ="";
+        var alt = "[{'login':'no.one@amdocs.com','sys_creation_date':'2017-05-19T20:24:46.000Z'},{'login':'no.one@amdocs.com','sys_creation_date':'2017-05-19T20:24:46.000Z'}]";
+        var workstr = "";
+        var payload = "";
+
+        if (accountType === 'pending') {
+            method = '/node/pending?appcode=amgate&mode=json';
+        }
+        else if (accountType === 'active') {
+            method = '/node/listactive?appcode=amgate&mode=json';
+        }
+        else if (accountType === 'locked') {
+            method = '/node/locked?appcode=amgate&mode=json';
+        }
+        else if (accountType === 'rejected') {
+            method = '/node/rejected?appcode=amgate&mode=json';
+        }
+
+        console.log("GET-METHOD" + method);
 
         var options = {
             host: 'amgate.itahs.com',
             //hostname: '10.26.56.70',
-            path: '/node/listactive?appcode=amgate&mode=json',
+            path: method,
             method: 'GET',
             agent: proxyAgent
         };
 
-        var optionsx = {
-            //host: 'amgate.itahs.com',
-            hostname: '10.26.56.70',
-            port: 443,
-            path: '/node/listactive?appcode=amgate&mode=json',
+        https.request(options, function (res) {
+
+            //console.log('STATUS: ' + res.statusCode);
+            //console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+            res.setEncoding('utf8');
+
+            res.on('data', function (chunk) {
+                workstr += chunk;
+                //console.log('ACTIVE-BODY: ' + chunk);
+            });
+
+            res.on('end', function () {
+
+                var jobj = JSON.parse(workstr);
+
+                if (jobj.status === "No Rows") {
+                    console.log("No Rows");
+                    payload = "";
+                }
+                else {
+                    payload = JSON.parse(workstr);
+                }
+
+                callback(payload);
+            })
+
+        }).end(function () {
+            console.log("GET-ACTIVE-END");
+        });
+    },
+
+    // common method for all actions ////////////////////////////////////////////////////////
+
+    updateAccount: function (account, action, callback) {
+
+        console.log("UPDATE-ACCOUNT");
+
+        var payload = "";
+
+        if (action === 'approve') {
+            method = '/node/activate?login=' + account + '&appcode=amgate';
+        }
+        else if (action === 'reject') {
+            method = '/node/reject?login=' + account + '&appcode=amgate';
+        }
+        else if (action === 'lock') {
+            method = '/node/lock?login=' + account + '&appcode=amgate';
+        }
+        else if (action === 'unlock') {
+            method = '/node/unlock?login=' + account + '&appcode=amgate';
+        }
+        else if (action === 'delete') {
+            method = '/node/delete?login=' + account + '&appcode=amgate';
+        }
+        else if (action === 'reset') {
+            method = '/node/reset?login=' + account + '&appcode=amgate';
+        }
+
+
+        console.log("UPDATE-METHOD:" + method);
+
+        var options = {
+
+            host: 'amgate.itahs.com',
+            //path: '/node/lock?login=' + account + '&appcode=amgate',
+            path: method,
             method: 'GET',
             agent: proxyAgent
         };
 
         https.request(options, function(res) {
 
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-
             res.setEncoding('utf8');
 
             res.on('data', function (chunk) {
                 payload += chunk;
-                console.log('ACTIVE-BODY: ' + chunk);
             });
 
             res.on('end', function() {
                 console.log("END");
-                callback(payload);
+                callback();
             })
 
         }).end(function() {
-            console.log("GET-ACTIVE-END");
+            console.log("END");
         });
+
     }
+
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var apis = require('../routes/apis');
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, callback, next) {
@@ -14,18 +15,17 @@ router.get('/', function(req, callback, next) {
             title: 'Express',
             tagline: tagline
         });
-
 });
 
 /* Present active users */
 router.get('/active', function(req, callback) {
 
-    apis.getActive( function(res) {
+    apis.getAccounts('active', function (res) {
 
         callback.render('active', {
             title: 'Active Mobile users',
-            message: "Showing all active mobile users",
-            payload : JSON.parse(res)
+            message: "Showing all active mobile users, you can lock an account from here",
+            payload: res
         });
     })
 });
@@ -33,18 +33,167 @@ router.get('/active', function(req, callback) {
 /* Present pending users list */
 router.get('/pending', function(req, callback) {
 
-    apis.getPending( function(res) {
+    apis.getAccounts('pending', function (res) {
 
         callback.render('pending', {
             title: 'Pending Mobile users',
             message: "Select users you want to approve or disaprove",
-            payload : JSON.parse(res)
+            payload: res
+        });
+    })
+});
+
+/* Present locked users list */
+router.get('/locked', function (req, callback) {
+
+    apis.getAccounts('locked', function (res) {
+
+        callback.render('locked', {
+            title: 'Locked Mobile users',
+            message: "Select users you want to unlock",
+            payload: res
+        });
+    })
+});
+
+/* Present locked users list */
+router.get('/rejected', function (req, callback) {
+
+    apis.getAccounts('rejected', function (res) {
+
+        callback.render('reject', {
+            title: 'Rejected Mobile users',
+            message: "Select users you want to delete or resubmit",
+            payload: res
         });
     })
 });
 
 router.get('/about', function(req, res, next) {
     res.render('about', { title: 'Express' });
+});
+
+router.post('/pendingUser', function (req, callback) {
+
+    var input = JSON.parse(JSON.stringify(req.body));
+
+    console.log(util.inspect(input, false, 4, true));
+
+    if (input.approve) {
+
+        delete input['approve'];
+
+        console.log("APPROVE INPUT:" + input.approve);
+
+        Object.keys(input).forEach(function (key) {
+            console.log("key:" + key + ":" + input[key]);
+            //https://amgate.itahs.com/node/activate?login=oded.levinstein@amdocs.com&appcode=amgate
+            apis.updateAccount(key, 'approve', function (res) {
+                console.log("Done");
+            });
+        });
+    }
+    else if (input.reject) {
+
+        console.log("REJECT INPUT:" + input.reject);
+
+        delete input['reject'];
+
+        Object.keys(input).forEach(function (key) {
+            console.log("key:" + key + ":" + input[key]);
+            // https://amgate.itahs.com/node/reject?login=xxx&appcode=amgate
+            apis.updateAccount(key, 'reject', function (res) {
+                console.log("Done");
+            });
+        });
+
+    }
+
+    callback.redirect('pending');
+});
+
+//LOCK: User account
+router.post('/activeUser', function (req, callback) {
+
+    var input = JSON.parse(JSON.stringify(req.body));
+
+    console.log(util.inspect(input, false, 4, true));
+
+    delete input['lock'];
+
+    console.log("LOCK INPUT:" + input.approve);
+
+    Object.keys(input).forEach(function (key) {
+        console.log("key:" + key + ":" + input[key]);
+        //https://amgate.itahs.com/node/activate?login=oded.levinstein@amdocs.com&appcode=amgate
+        apis.updateAccount(key, 'lock', function (res) {
+            console.log("Done");
+        });
+    });
+
+    callback.redirect('active');
+});
+
+// LOCK: user account
+router.post('/lockUser', function (req, callback) {
+
+    var input = JSON.parse(JSON.stringify(req.body));
+
+    console.log(util.inspect(input, false, 4, true));
+
+    console.log("UNLOCK INPUT:" + input.lock);
+
+    delete input['unlock'];
+
+    Object.keys(input).forEach(function (key) {
+        console.log("key:" + key + ":" + input[key]);
+        // https://amgate.itahs.com/node/reject?login=xxx&appcode=amgate
+        apis.updateAccount(key, 'unlock', function (res) {
+            console.log("Done");
+        });
+    });
+
+    callback.redirect('locked');
+});
+
+
+router.post('/rejectUser', function (req, callback) {
+
+    var input = JSON.parse(JSON.stringify(req.body));
+
+    console.log(util.inspect(input, false, 4, true));
+
+    if (input.delete) {
+
+        delete input['delete'];
+
+        console.log("DELETE INPUT:" + input.delete);
+
+        Object.keys(input).forEach(function (key) {
+            console.log("key:" + key + ":" + input[key]);
+            //https://amgate.itahs.com/node/activate?login=oded.levinstein@amdocs.com&appcode=amgate
+            apis.updateAccount(key, 'delete', function (res) {
+                console.log("Done");
+            });
+        });
+    }
+    else if (input.reset) {
+
+        console.log("RESET INPUT:" + input.reset);
+
+        delete input['reset'];
+
+        Object.keys(input).forEach(function (key) {
+            console.log("key:" + key + ":" + input[key]);
+            // https://amgate.itahs.com/node/reject?login=xxx&appcode=amgate
+            apis.updateAccount(key, 'reset', function (res) {
+                console.log("Done");
+            });
+        });
+
+    }
+
+    callback.redirect('rejected');
 });
 
 module.exports = router;
